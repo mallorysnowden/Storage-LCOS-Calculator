d1005e018f3b21a1eadd884bc17e427bfbe926d3
msnowden729-alt <msnowden729@gmail.com>
2025-11-15 17:32:27 -0800

    Update BESScalcs.py
    deleted the .py export step

--- BESScalcs.py
+++ BESScalcs.py
@@ -1,34 +1,10 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[89]:
-
-
-import nbformat
-from nbconvert import PythonExporter
 import os
 import matplotlib.pyplot as plt
 import numpy as np
 import math
-from scipy.optimize import curve_fit
+from scipy.optiminbze import curve_fit
 
-
-# In[90]:
-
-
-# Convert notebook to Python script
-notebook_path = 'BESScalcs.ipynb'
-output_path = 'BESScalcs.py'
-
-with open(notebook_path, 'r', encoding='utf-8') as f:
-    notebook = nbformat.read(f, as_version=4)
-
-exporter = PythonExporter()
-python_code, _ = exporter.from_notebook_node(notebook)
-with open(output_path, 'w', encoding='utf-8') as f:
-    f.write(python_code)
-
-print(f"Exported {notebook_path} to {output_path}")
 
 ## Main Program ##
 def run(inputs: dict) -> dict:
@@ -294,6 +270,7 @@ print(f'New LCOS = ${results['newLCOS']:0.3f}/kWh') """
 
 
 # In[ ]:
+
 
 
 
